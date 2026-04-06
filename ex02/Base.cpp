4efec2138f4b3ed79ed95feed877c6acce285274
michel_32 <karas.benjamin@gmail.com>
2026-04-06 17:43:16 +0200

    implement generate()

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -1,19 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 17:11:18 by michel_32         #+#    #+#             */
-/*   Updated: 2026/04/06 17:11:40 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/04/06 17:42:27 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
-#include <iostream>
 
 Base::~Base(void)
 {
     std::cout << "Base destructor called" << std::endl;
+}
+
+// Base *generate(void);
+// void identify(Base* p);
+// void identify(Base& p);
+
+Base *generate(void)
+{
+    int i = std::rand() % 3;
+
+    if (i == 0)
+    {
+        std::cout << "Generated A" << std::endl;
+        return (new A());
+    }
+    else if (i == 1)
+    {
+        std::cout << "Generated B" << std::endl;
+        return (new B());
+    }
+    else
+    {
+        std::cout << "Generated C" << std::endl;
+        return (new C());
+    }
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/06 17:12:11 by michel_32         #+#    #+#             */
-/*   Updated: 2026/04/06 17:42:22 by michel_32        ###   ########.fr       */
+/*   Created: 2026/04/06 17:41:52 by michel_32         #+#    #+#             */
+/*   Updated: 2026/04/06 17:42:59 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BASE_HPP
-#define BASE_HPP
+#include "Base.hpp"
 
-#include <cstdlib> // For std::rand() and std::srand()
-#include <ctime>   // For std::time()
-#include "A.hpp"
-#include "B.hpp"
-#include "C.hpp"
-#include <iostream>
-
-class Base {
-public:
-
-    ~Base(void);
-};
-
-Base *generate(void);
-void identify(Base* p);
-void identify(Base& p);
-
-#endif // BASE_HPP
+int main (void)
+{
+    std::srand(std::time(0)); //seeding a nice plant
+}
